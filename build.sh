d6627e14c31466c31bd189793b0869d4d040b3f5
BugenZhao <i@bugenzhao.com>
2021-06-23 17:40:13 +0800

    *: xcode build script

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-echo ">>>>> Rust"
-cargo build --manifest-path rust/Cargo.toml --release
-cp rust/target/release/librust.a out/libs
-cp rust/bindings.h out/include
-
 echo ">>>>> Swift PB"
 protoc --swift_out=swift/InteropApp/InteropApp/ -I protos/ protos/DataModel.proto
+
+echo ">>>>> Rust"
+$HOME/.cargo/bin/cargo build --manifest-path rust/Cargo.toml --release
+cp rust/target/release/librust.a out/libs
+cp rust/bindings.h out/include
